622134d63d96f2ab9b3a8ef8f836aa0e75c50678
nashemistoart@gmail.com <nashemistoart@gmail.com>
2022-01-19 18:28:51 +0200

    add exeption Update and Dell

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -13,14 +13,14 @@ public class Main {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
         String strConn = "jdbc:mariadb://localhost:3306/javacrud1";
-        InsertIntoDB(strConn);
+        //InsertIntoDB(strConn);
         List<Product> list = SelectFromDB(strConn);
         PrintProductList(list);
-//        UpdateForDB(strConn);
-//        list = SelectFromDB(strConn);
-//        PrintProductList(list);
-        DeleteFromDB(strConn);
-        PrintProductList(SelectFromDB(strConn));
+        UpdateForDB(strConn);
+        list = SelectFromDB(strConn);
+        PrintProductList(list);
+//        DeleteFromDB(strConn);
+//        PrintProductList(SelectFromDB(strConn));
     }
 
     private static void PrintProductList(List<Product> prods) {
@@ -87,22 +87,32 @@ public class Main {
     }
 
     private static void UpdateForDB(String strConn) {
-        try(Connection con = DriverManager.getConnection(strConn, "root", "")) {
+        int id;
+        boolean boolID = false;
+        try (Connection con = DriverManager.getConnection(strConn, "root", "")) {
             String query = "UPDATE products SET name = ? WHERE id = ?";//запрос на обновление названия продукта по ID
-            try(PreparedStatement stmt = con.prepareStatement(query)) {
-                System.out.print("\nEnter id: ");
-                int id = in.nextInt();
+            try (PreparedStatement stmt = con.prepareStatement(query)) {
+                while (!boolID) {
+                    try {
+                        System.out.print("Enter Id: ");
+                        id = Integer.parseInt(in.nextLine());
+
+                        stmt.setInt(2, id);
+                        boolID = true;
+                    } catch (Exception e) {
+                        System.out.println("Ошибка выбора операции. Введите целое число!");
+                    }
+                }
+
                 System.out.print("Enter new name: ");
-                in.nextLine();
+
                 String name = in.nextLine();
                 stmt.setString(1, name);//вносятся параметры
-                stmt.setInt(2, id);
 
                 int rows = stmt.executeUpdate();//выполняется запрос
 
-                System.out.println("Successful update "+ rows);
-            }
-            catch (Exception ex) {
+                System.out.println("Successful update " + rows);
+            } catch (Exception ex) {
                 System.out.println("Error update:" + ex.getMessage());
             }
         } catch (Exception ex) {
@@ -111,13 +121,19 @@ public class Main {
     }
 
     private static void DeleteFromDB(String strConn) {
-        //SelectFromDB(strConn);
-        try(Connection con = DriverManager.getConnection(strConn, "root", "")) {
+        int id;
+        boolean bool = false;
+       try(Connection con = DriverManager.getConnection(strConn, "root", "")) {
             String query = "DELETE FROM products WHERE id = ?";
             try(PreparedStatement stmt = con.prepareStatement(query)) {
-                System.out.print("Enter Id: ");
-                int id = in.nextInt();
+                while (!bool) {
+                try {System.out.print("Enter Id: ");
+                    id = Integer.parseInt(in.nextLine());
+
                 stmt.setInt(1, id);
+                    bool = true;}
+                catch (Exception e) {
+                    System.out.println("Ошибка выбора операции. Введите целое число!");}}
                 int rows = stmt.executeUpdate();
                 System.out.println("Successful delete " + rows);
 
